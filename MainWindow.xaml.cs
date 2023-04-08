fa1c413534baf28f4a1e07ef1c3900b8f2a1257a
Thomas Radigk <t.radigk@outlook.de>
2023-04-08 10:15:04 +0200

    Not setting IsResizable -> Window has corner radius

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,21 +3,7 @@
 
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Windowing;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -35,12 +21,9 @@ namespace WindowCornerRadiusBug
             this.InitializeComponent();
             var appWindow = GetAppWindowFromWindow(this);
             appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
-            (appWindow.Presenter as OverlappedPresenter).SetBorderAndTitleBar(false, false);
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
+            var windowPresenter = (appWindow.Presenter as OverlappedPresenter);
+            windowPresenter.SetBorderAndTitleBar(true, false);
+            //windowPresenter.IsResizable = false;
         }
 
         public static AppWindow GetAppWindowFromWindow(Window windowRef)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WindowCornerRadiusBug
+
+This "app" demonstrates, that with a window object, that has "ExtendsContentIntoTileBar" and "IsResizable = false" the corner radius is lost.
